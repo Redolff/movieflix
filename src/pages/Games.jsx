47b6b5e4dcefe9397a373079e3105d2cc51647d7
Redolff <redolfofederico@gmail.com>
2025-08-25 14:20:34 -0300

    new games filters with platform

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -5,6 +5,7 @@ import { Carrousels } from "../components/Carrousels"
 export const Games = () => {
     const { data: allGames } = useFetchData("games")
     const { data: ps4Games } = useFetchData("games", { platform: "ps4" })
+    const { data: pcGames } = useFetchData("games", { platform: "pc" })
 
     return (
         <div>
@@ -17,6 +18,11 @@ export const Games = () => {
                 title={`Todos los juegos para PS4`}
                 items={ps4Games}
                 renderItem={(ps4Games) => <Game game={ps4Games} key={ps4Games.id} />}
+            />
+            <Carrousels
+                title={`Todos los juegos para PC`}
+                items={pcGames}
+                renderItem={(pcGames) => <Game game={pcGames} key={pcGames.id} />}
             />
         </div>
     )
